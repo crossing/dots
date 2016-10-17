454e2ee48efaf8088f404eeca12169e2f7afef58
Xing Yang <xing.yang@klarna.com>
2016-10-17 12:28:30 +0200

    Add pyenv virtualenv plugin.

--- shell/.zshrc.d/python.sh
+++ shell/.zshrc.d/python.sh
@@ -2,4 +2,8 @@ if [ ! -d $HOME/.pyenv ]; then
   git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 fi
 
+if [ ! -d $HOME/.pyenv/plugins/pyenv-virtualenv ]; then
+  git clone https://github.com/yyuu/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
+fi
+
 plugins+=(pyenv python pip virtualenv)
