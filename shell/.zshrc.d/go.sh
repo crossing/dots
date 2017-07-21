d2de2df29fb806ca93e4198122e3879c5f185caa
Xing Yang <xor@jecity.net>
2017-07-21 20:56:37 +0100

    Remove golang tooling.

--- shell/.zshrc.d/go.sh
+++
@@ -1,6 +0,0 @@
-if [ ! -d $HOME/.gvm ]; then
-  zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-fi
-
-. $HOME/.gvm/scripts/gvm
-plugins+=(golang)
