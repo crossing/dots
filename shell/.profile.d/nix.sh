7232c98ea347b1a20cadc255b4f9f485c10cb0bb
Xing Yang <xor@jecity.net>
2017-11-14 16:37:08 +0000

    Install nix if not already installed.

--- shell/.profile.d/nix.sh
+++ shell/.profile.d/nix.sh
@@ -1,3 +1,7 @@
+if [ ! -d /nix/store ]; then
+    curl https://nixos.org/nix/install | sh
+fi
+
 if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
   . $HOME/.nix-profile/etc/profile.d/nix.sh;
 fi # added by Nix installer
