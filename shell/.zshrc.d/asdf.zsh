ddffe5cf208be956c7f39e71a7e510340d7311f4
Xing Yang <xing.yang@quantinfotech.com>
2020-02-21 12:37:22 +0000

    Use asdf

---
+++ shell/.zshrc.d/asdf.zsh
@@ -0,0 +1,30 @@
+if [ ! -d "$HOME/.asdf" ]; then
+    git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf
+    pushd $HOME/.asdf
+    git checkout "$(git describe --abbrev=0 --tags)"
+    popd
+
+    ASDF_INIT=true
+fi
+
+_omz_plugin asdf
+
+adsf_plugins=(
+    java
+    clojure
+
+    python
+    ruby
+    rust
+    golang
+
+    haskell
+    idris
+
+    direnv
+    terraform
+    packer
+)
+for plugin in $adsf_plugins; do
+    eval "asdf plugin add $plugin" 2>/dev/null
+done
