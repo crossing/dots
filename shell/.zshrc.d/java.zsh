5c049f6d6b2cf806f9c0fe6abd71eb2e6106c2a3
Xing Yang <xing.yang@quantinfotech.com>
2020-02-21 12:46:33 +0000

    Manage java via asdf

--- shell/.zshrc.d/java.zsh
+++ shell/.zshrc.d/java.zsh
@@ -1,9 +1,3 @@
-if [ ! -d "$HOME/.sdkman" ]; then
-    curl -s "https://get.sdkman.io" | bash
-fi
-
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-
 # Set JAVA_HOME
 JRE=$(readlink -f $(which java))
 JDK=$(readlink -f $(which javac))
