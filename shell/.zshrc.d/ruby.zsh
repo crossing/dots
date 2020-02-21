4540f55cfa8638a505c32fbd903cb4a0daffbb67
Xing Yang <xing.yang@quantinfotech.com>
2020-02-21 12:54:38 +0000

    Use asdf to manage ruby

--- shell/.zshrc.d/ruby.zsh
+++ shell/.zshrc.d/ruby.zsh
@@ -1,12 +1,2 @@
-if [ ! -d $HOME/.rbenv ]; then
-    git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
-fi
-
-if [ ! -d $HOME/.rbenv/plugins/ruby-build ]; then
-    git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-build
-fi
-
-_omz_plugin rbenv
 _omz_plugin ruby
 _omz_plugin bundler
-
