2303d27edd29e20f9fb941873ca7808984706d6b
Xing Yang <xing.yang@quantinfotech.com>
2020-04-28 17:46:02 +0100

    reinstate pipx

--- dot_zshrc.d/20-python.sh
+++ dot_zshrc.d/20-python.sh
@@ -2,8 +2,14 @@
 _omz_plugin python
 _omz_plugin pip
 
+# Pipx
+[ -f $HOME/.local/bin/pipx ] || python3 -m pip install --user pipx argcomplete
+eval "$(register-python-argcomplete pipx)"
+
 # Conda
 if [ -f $HOME/.conda/etc/profile.d/conda.sh ]; then
     source $HOME/.conda/etc/profile.d/conda.sh
     antibody bundle bckim92/zsh-autoswitch-conda
 fi
+
+export PYTHONWARNINGS=ignore
--- dot_zshrc.d/docker.zsh
+++ dot_zshrc.d/docker.zsh
@@ -1,1 +1,5 @@
+# docker-compose
+which docker-compose > /dev/null || pipx install docker-compose
+
 _omz_plugin docker
+_omz_plugin docker-compose
