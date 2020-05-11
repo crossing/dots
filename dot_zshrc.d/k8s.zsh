55750e7f5ac4529114a57eb67b6f7f004f3572e6
杨星 <xing.yang@quantinfotech.com>
2020-05-11 12:00:06 +0100

    Install more k8s tools.

--- dot_zshrc.d/k8s.zsh
+++ dot_zshrc.d/k8s.zsh
@@ -1,6 +1,11 @@
-_asdf_tool kubectl
-_asdf_tool minikube
-_asdf_tool skaffold
+_asdf_tool kubectl 
+_asdf_tool minikube 
+_asdf_tool skaffold 
+_asdf_tool fluxctl 
+_asdf_tool eksctl 
 
 _omz_plugin kubectl
 _omz_plugin minikube
+
+source <(skaffold completion zsh)
+source <(fluxctl completion zsh)
