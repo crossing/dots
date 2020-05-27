eb6369dfc42297d778ff1fd2ec2d98fa3b72d272
杨星 <xing.yang@quantinfotech.com>
2020-05-27 19:17:52 +0100

    Add helm / bump minikube

--- dot_zshrc.d/k8s.zsh
+++ dot_zshrc.d/k8s.zsh
@@ -1,11 +1,13 @@
 _asdf_tool kubectl 1.18.2
-_asdf_tool minikube 1.10.0
+_asdf_tool minikube 1.10.1
 _asdf_tool skaffold 1.9.1
 _asdf_tool fluxctl 1.19.0
 _asdf_tool eksctl 0.18.0
+_asdf_tool helm 3.2.1
 
 _omz_plugin kubectl
 _omz_plugin minikube
+_omz_plugin helm
 
 source <(skaffold completion zsh)
 source <(fluxctl completion zsh)
