a69043b88800f3f8bc3f0919122ba212ad7c6073
杨星 <xing.yang@quantinfotech.com>
2020-05-30 17:10:14 +0100

    Update k8s tooling

--- dot_zshrc.d/k8s.zsh
+++ dot_zshrc.d/k8s.zsh
@@ -1,8 +1,8 @@
 _asdf_tool kubectl 1.18.2
 _asdf_tool minikube 1.10.1
-_asdf_tool skaffold 1.9.1
+_asdf_tool skaffold 1.10.1
 _asdf_tool fluxctl 1.19.0
-_asdf_tool eksctl 0.18.0
+_asdf_tool eksctl 0.20.0
 _asdf_tool helm 3.2.1
 
 _omz_plugin kubectl
@@ -11,3 +11,5 @@ _omz_plugin helm
 
 source <(skaffold completion zsh)
 source <(fluxctl completion zsh)
+
+eksctl completion zsh > $HOME/.zfunc/_eksctl
