b743ea358bd7a7b0cf7bd2eb2204e337320597b8
杨星 <xing.yang@quantinfotech.com>
2020-05-30 13:58:18 +0100

    Update aws-vault configuration

--- dot_zshrc.d/aws.zsh
+++ dot_zshrc.d/aws.zsh
@@ -3,15 +3,10 @@ which aws > /dev/null 2>&1 || pipx install awscli
 _omz_plugin aws
 
 # aws-vault
-AWS_VAULT_VERSION=5.2.1
+AWS_VAULT_VERSION=5.4.4
 if ! ( which aws-vault > /dev/null 2>&1 &&
        aws-vault --version | grep -w "v${AWS_VAULT_VERSION}" > /dev/null ); then
     curl -L https://github.com/99designs/aws-vault/releases/download/v${AWS_VAULT_VERSION}/aws-vault-linux-amd64 -o $HOME/.local/bin/aws-vault
     chmod +x $HOME/.local/bin/aws-vault
 fi
 antibody bundle blimmer/zsh-aws-vault
-
-alias aw='aws-vault'
-alias awe='aws-vault exec'
-alias awen='aws-vault exec -n'
-alias awl='aws-vault login'
