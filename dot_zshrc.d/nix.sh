27397fe4036cfd60fb1007192f904bdee92f83eb
Xing Yang <xor@jecity.net>
2021-04-24 18:11:17 +0100

    Ensure home-manager is installed.

--- dot_zshrc.d/nix.sh
+++ dot_zshrc.d/nix.sh
@@ -6,6 +6,14 @@ if [ -f /etc/profile.d/nix.sh ]; then
     compinit
 fi
 
-if [ -f $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh ]; then
-    source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh 
+if $((+commands[nix])); then
+    if [ -f $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh ]; then
+        source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh 
+    fi
+
+    if ! $((+commands[home-manager])) then
+        nix-channel --add https://github.com/nix-community/home-manager/archive/release-20.09.tar.gz home-manager
+        nix-channel --update
+        nix-shell '<home-manager>' -A install
+    fi
 fi
