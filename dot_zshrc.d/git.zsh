a224defce15196d30b06de77bc7618dff6cc9183
Xing Yang <xor@jecity.net>
2021-04-24 11:44:49 +0100

    Use nix home manager to install github cli.

--- dot_zshrc.d/git.zsh
+++ dot_zshrc.d/git.zsh
@@ -1,12 +1,2 @@
 _zgen_omz git
 _zgen_omz git-extras
-
-_asdf_tool github-cli 1.8.1
-
-function gh() {
-    unfunction gh
-    gh completion --shell zsh > $HOME/.zfunc/_gh
-    compinit
-
-    gh $@
-}
--- dot_zshrc.d/nix.sh
+++ dot_zshrc.d/nix.sh
@@ -1,6 +1,11 @@
 if [ -f /etc/profile.d/nix.sh ]; then
     source /etc/profile.d/nix.sh
+    export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
     _zgen_load chisui/zsh-nix-shell
     _zgen_load spwhitt/nix-zsh-completions
     compinit
+fi
+
+if [ -f $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh ]; then
+    source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh 
 fi
