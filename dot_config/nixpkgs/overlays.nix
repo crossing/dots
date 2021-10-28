204b9a0eec47561d6cd0f65fdf170800d69b6dde
Xing Yang <xor@jecity.net>
2021-10-28 22:05:39 +0100

    No need to override awscli2 anymore.

--- dot_config/nixpkgs/overlays.nix
+++ dot_config/nixpkgs/overlays.nix
@@ -3,17 +3,6 @@ let
 in
 [
   (self: super: {
-    awscli2 = super.awscli2.overridePythonAttrs (old: {
-      postPatch = ''
-        ${if old ? postPatch then old.postPatch else ""}
-
-        substituteInPlace setup.py \
-          --replace 's3transfer>=0.4.2,<0.5.0' 's3transfer'
-      '';
-
-      doCheck = false;
-    });
-
     oidc-cli = (import sources.oidc-cli { });
 
     glab = super.glab.overrideAttrs (old: {
