131183bd91d038cc429c2a6b6d4454fdd67c09d4
Xing Yang <xor@jecity.net>
2022-06-26 10:43:43 +0100

    Manage .gitconfig via home manager.

--- dot_config/nixpkgs/git.nix
+++ dot_config/nixpkgs/git.nix
@@ -5,7 +5,19 @@
     package = pkgs.git.override {
       withLibsecret = true;
     };
+
+    userName = "Xing Yang";
+    userEmail = "xor@jecity.net";
+
+    lfs.enable = true;
+
+    extraConfig = {
+      credential.helper = "libsecret";
+    };
   };
 
-  programs.gh.enable = true;
+  programs.gh = {
+    enable = true;
+    enableGitCredentialHelper = true;
+  };
 }
